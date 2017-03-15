69efd6ed22ce570a50f479548ea2b284c9218a2f
René Nitzsche <rene@system25.de>
2017-03-15 11:38:49 +0100

    Changes for TYPO3 7.6

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,6 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
-require_once(t3lib_extMgm::extPath('rn_base') . 'class.tx_rnbase.php');
 tx_rnbase::load('tx_rnbase_util_SearchBase');
 tx_rnbase::load('tx_rnbase_util_TYPO3');
 
@@ -10,13 +9,13 @@ tx_rnbase::load('tx_rnbase_util_TYPO3');
 // -------------------------
 tx_rnbase::load('tx_more4t3sports_srv_Registry');
 if(tx_rnbase_util_TYPO3::isExtLoaded('t3socials')) {
-	t3lib_extMgm::addService($_EXTKEY,  't3sports_srv' /* sv type */,  'tx_more4t3sports_srv_Socials' /* sv key */,
+	tx_rnbase_util_Extensions::addService($_EXTKEY,  't3sports_srv' /* sv type */,  'tx_more4t3sports_srv_Socials' /* sv key */,
 		array(
 			'title' => 'Social networks', 'description' => 'Handles communications with social networks',
 			'subtype' => 'socials',
 			'available' => TRUE, 'priority' => 50, 'quality' => 50,
 			'os' => '', 'exec' => '',
-			'classFile' => t3lib_extMgm::extPath($_EXTKEY).'srv/class.tx_more4t3sports_srv_Socials.php',
+				'classFile' => tx_rnbase_util_Extensions::extPath($_EXTKEY).'srv/class.tx_more4t3sports_srv_Socials.php',
 			'className' => 'tx_more4t3sports_srv_Socials',
 		)
 	);
--- ext_tables.php
+++ ext_tables.php
@@ -4,11 +4,9 @@ if (!defined ('TYPO3_MODE')) {
 }
 
 tx_rnbase::load('tx_rnbase_util_TYPO3');
-// TODO: Nach umstellung der TCA in cfc_league hier auf die T3-Version prüfen
-//if(!tx_rnbase_util_TYPO3::isTYPO62OrHigher()) {
-	tx_rnbase::load('tx_rnbase_util_Extensions');
+if(!tx_rnbase_util_TYPO3::isTYPO62OrHigher()) {
 	require tx_rnbase_util_Extensions::extPath($_EXTKEY).'Configuration/TCA/Overrides/tx_cfcleague_games.php';
-//}
+}
 
 
 tx_rnbase_util_Extensions::addStaticFile($_EXTKEY,'Configuration/Typoscript/News/', 'T3sports with tt_news');
